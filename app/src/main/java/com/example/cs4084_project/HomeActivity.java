5a36638428e0771140fbeda7e91bce0df9ae184d
TZajas <20278748@studentmail.ul.ie>
2023-03-07 16:37:52 +0000

    Added login and register functionality

--- app/src/main/java/com/example/cs4084_project/HomeActivity.java
+++ app/src/main/java/com/example/cs4084_project/HomeActivity.java
@@ -1,15 +1,50 @@
 package com.example.cs4084_project;
 
-        import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
-
-
+    FirebaseAuth auth;
+    Button logoutButton;
+    TextView detailsDisplay;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_main);
+        setContentView(R.layout.activity_home);
+
+        auth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logout_btn);
+        detailsDisplay = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        if(user==null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            detailsDisplay.setText(user.getEmail());
+        }
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/cs4084_project/SplashScreen.java
+++ app/src/main/java/com/example/cs4084_project/SplashScreen.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
@@ -18,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, HomeActivity.class));
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
                 finish();
             }
         },4000);
